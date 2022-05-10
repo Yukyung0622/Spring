cd609c0a1036f22d7ce661b9d12f66433228e397
unknown <kkomang0622@gmail.com>
2022-05-10 17:01:11 +0900

    Kmarket

--- Kmarket/src/main/java/kr/co/kmarket/dao/ProductDao.java
+++ Kmarket/src/main/java/kr/co/kmarket/dao/ProductDao.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
-import kr.co.kmarket.admin.vo.ProductVo;
+import kr.co.kmarket.vo.CategoriesVo;
+import kr.co.kmarket.vo.ProductVo;
 
 @Mapper
 @Repository
 public interface ProductDao {
 	
+	
+	public ProductVo selectProduct(int pid);
 	public List<ProductVo> selectProducts(ProductVo vo);
+	public CategoriesVo selectCateTitles(ProductVo vo);
 
+	
 }
--- Kmarket/src/main/java/kr/co/kmarket/service/ProductService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/ProductService.java
@@ -5,17 +5,25 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.kmarket.admin.vo.ProductVo;
 import kr.co.kmarket.dao.ProductDao;
+import kr.co.kmarket.vo.CategoriesVo;
+import kr.co.kmarket.vo.ProductVo;
 
 @Service
 public class ProductService {
-	
+
 	@Autowired
 	private ProductDao dao;
+	
+	public ProductVo selectProduct(int pid) {
+		return dao.selectProduct(pid);
+	}
 	
 	public List<ProductVo> selectProducts(ProductVo vo){
 		return dao.selectProducts(vo);
 	}
-
+	
+	public CategoriesVo selectCateTitles(ProductVo vo) {
+		return dao.selectCateTitles(vo);
+	}
 }
